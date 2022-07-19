c59399dd5bd47f20c7aab307ede87fc40835e4f7
Guillermo <palmierigui87@gmail.com>
2022-07-18 21:01:53 -0300

    explaboral

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { SocialComponent } from './components/social/social.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperiencialaboralComponent } from './components/experiencialaboral/experiencialaboral.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     SocialComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperiencialaboralComponent
   ],
   imports: [
     BrowserModule,
---
+++ proyecto/src/app/components/experiencialaboral/experiencialaboral.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencialaboral',
+  templateUrl: './experiencialaboral.component.html',
+  styleUrls: ['./experiencialaboral.component.css']
+})
+export class ExperiencialaboralComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
