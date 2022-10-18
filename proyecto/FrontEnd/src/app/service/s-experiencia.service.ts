454e30f80a898e713aa2698741a033974dacf52f
Guillermo <palmierigui87@gmail.com>
2022-10-18 16:43:16 -0300

    final

--- proyecto/FrontEnd/src/app/service/s-experiencia.service.ts
+++ proyecto/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -7,7 +7,7 @@ import { Experiencia } from '../model/experiencia';
   providedIn: 'root'
 })
 export class SExperienciaService {
-expURL = 'http://localhost:8080/explab/'
+expURL = 'https://backendpalmierigui.herokuapp.com/explab/'
   constructor(private httpClient:HttpClient) { }
 
   public lista(): Observable<Experiencia[]>{
